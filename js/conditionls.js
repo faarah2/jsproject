16fcbdcb0c6edc7bed61e00257164a3f7dead61f
Admin <Admin@DESKTOP-KLPHNKH>
2024-10-18 17:38:30 +0300

    init commint

---
+++ js/conditionls.js
@@ -0,0 +1,7 @@
+function showName(){
+var x = document.getElementById("txt").value;
+var y = document.getElementById("txt2").value;
+alert(x*y); 
+
+
+}
---
+++ js/main.js
@@ -0,0 +1,22 @@
+//alert('Hello this is test');
+
+/*
+console.log("Hey this is JS");
+console.log('My name is Ahmed');
+document.getElementById('test').innerHTML = 'Ahmed';
+
+*/
+
+var x = 5;
+
+//alert(x);
+
+
+
+var name = "Ahmed";
+alert(name)
+
+
+
+
+
